014596814b48e5e345a36e28f9466eb4dd897835
ccoreil14 <ccoreil14@gmail.com>
2016-10-22 11:46:37 -0700

    a

--- code/assets/scripts/main.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu(boxInsidesId){
-    var dropdown = document.getElementById(boxInsidesId);
-    if (dropdown.style.visibility === 'collapse'){
-        console.log("visible "+boxInsidesId);
-        dropdown.style.visibility = 'visible';
-    }else{
-        dropdown.style.visibility = 'collapse';
-    }
-}
