810ec368cc914b9b5eeed3abde01e1fa0cb86132
ccoreil14 <ccoreil14@gmail.com>
2016-10-22 14:30:41 -0700

    made so that socket io is off while trying to get this up and running

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //Dependencies
 var app = require('express')();
-var http = require('http').Server(app);
+//var http = require('http').Server(app);
 var path = require('path');
-var sio = require('socket.io')(http); //We pass in the instance of our http server to socket io
+app.set('port', (process.env.PORT || 3000));
+//var sio = require('socket.io')(app.get('port')); //We pass in the instance of our http server to socket io
+
 
 //Public folder creation
 app.use(require('express').static(path.join(__dirname, 'assets')));
@@ -20,16 +22,23 @@ app.get('/', function (req, res) {
 ////Socket Code goes here////
 /////////////////////////////
 
-sio.on('connect', function (socket) {
-    socket.on('message-sent', function (msg) {
-        sio.emit('send-to-client', msg);
-    });
+//sio.on('connect', function (socket) {
+//    socket.on('message-sent', function (msg) {
+//        sio.emit('send-to-client', msg);
+//    });
+//});
+
+
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
 });
 
 
 
-//Listening
-http.listen(3000, function () {
-    console.log('Server listening on 3000...');
-});
 
+//Listening locally
+
+//http.listen(3000, function () {
+//    console.log('Server listening on 3000...');
+//});
