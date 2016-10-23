879b936feee837b0a7290df7586f9c93153c998c
ccoreil14 <ccoreil14@gmail.com>
2016-10-23 16:13:31 -0700

    Fixed yo

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,3 +7,35 @@ function toggleMenu(boxInsidesId){
         dropdown.style.visibility = 'collapse';
     }
 }
+
+function rectArea(x,y) {
+    return x*y;
+}
+
+function perimeter(x,y) {
+    return (x*2)+(y*2);
+}
+
+function circArea(r) {
+    return 3.14 * (r*r);
+}
+
+function circumfrence(r) {
+    return 3.14 * (r*2);
+}
+
+function prismSurfaceArea(x,y,z) {
+    return 2*((x*y)+(y*z)+(z*x));
+}
+
+function prismVolume(x,y,z) {
+    return (x*y*z);
+}
+
+function sphereSurfaceArea(r) {
+    return (4*3.14) * (r*r);
+}
+
+function sphereVolume(r) {
+    return ((4/3)*3.14) * (r*r*r);
+}
--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 //Dependencies
 var app = require('express')();
-//var http = require('http').Server(app);
+var server = require('http').Server(app);
 var path = require('path');
-//var sio = require('socket.io')(http); //We pass in the instance of our http server to socket io
+var sio = require('socket.io')(server); //We pass in the instance of our http server to socket io
 
-//Public folder creation
+//Listening
+server.listen(process.env.PORT || 5000);
+
+////Public folder creation
 app.use(require('express').static(path.join(__dirname, 'assets')));
 
 
-///////////////
-////ROUTING////
-///////////////
+/////////////////
+//////ROUTING////
+/////////////////
 app.get('/', function (req, res) {
+    console.log("made it to home page");
     res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-
-/////////////////////////////
-////Socket Code goes here////
-/////////////////////////////
-
-//sio.on('connect', function (socket) {
-//    socket.on('message-sent', function (msg) {
-//        sio.emit('send-to-client', msg);
-//    });
-//});
 //
-
-
-//Listening
-http.listen(3000, function () {
-    console.log('Server listening on 3000...');
+///////////////////////////////
+//////Socket Code goes here////
+///////////////////////////////
+//
+sio.on('connect', function (socket) {
+    socket.on('message-sent', function (msg) {
+        sio.emit('send-to-client', msg);
+    });
 });
-
