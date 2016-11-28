f864925e8c76176ccd172e26e674b8d08706516b
ccoreil14 <ccoreil14@gmail.com>
2016-11-28 11:09:49 -0800

    g

--- Public/routes.js
+++ Public/routes.js
@@ -31,7 +31,7 @@ module.exports = function (app, passport, path, pathYO) {
     });
 
     app.post('/login', passport.authenticate('local-login', {
-        successRedirect: '/profile', // redirect to the secure profile section
+        successRedirect: '/home', // redirect to the secure profile section
         failureRedirect: '/login', // redirect back to the signup page if there is an error
         failureFlash: true // allow flash messages
     }));
@@ -39,7 +39,7 @@ module.exports = function (app, passport, path, pathYO) {
 
     // process the signup form
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/profile', // redirect to the secure profile section
+        successRedirect: '/home', // redirect to the secure profile section
         failureRedirect: '/signup', // redirect back to the signup page if there is an error
         failureFlash: true // allow flash messages
     }));
